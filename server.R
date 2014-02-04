2966b1b0ecb2d3c8f2f9d383f7d9cf330e042de8
asmith3 <baylorAsmith@gmail.com>
2014-02-04 02:01:22 +0000

    Changed plot type radio labels

--- server.R
+++ server.R
@@ -10,20 +10,20 @@ shinyServer(function(input, output) {
   
   output$chart <- renderChart({
     
-    if(input$radio == "Yields") {
+    if(input$radio == "Yields of Funds") {
       if(input$time == "5 Years"){subdata <- tspdata[,18:22][seq(1, nrow(tspdata), by=21),]; 
                                   labels<-c(fiveyearlabels,fiveyearlabels,fiveyearlabels,fiveyearlabels,fiveyearlabels,"J")
                                   
                                   c <- Highcharts$new()
-                                  c$chart(type = "spline", height="450", width="800", zoomType="x")
+                                  c$chart(type = "spline", height="500", width="900", zoomType="x")
                                   c$title(text = paste("TSP ", input$radio, ", Past ", input$time))
                                   c$set(dom = "chart")
                                   
                                   c$xAxis(title = list(text = "Month"), categories = labels
-                                          , plotLines = (list(list(value=12, color='#B7CEEC', width=.75, dashStyle='longdash'),
-                                                              list(value=24, color='#B7CEEC', width=.75, dashStyle='longdash'),
-                                                              list(value=36, color='#B7CEEC', width=.75, dashStyle='longdash'),
-                                                              list(value=48, color='#B7CEEC', width=.75, dashStyle='longdash')
+                                          , plotLines = (list(list(value=12, color='#A0A0A0', width=.75, dashStyle='longdash'),
+                                                              list(value=24, color='#A0A0A0', width=.75, dashStyle='longdash'),
+                                                              list(value=36, color='#A0A0A0', width=.75, dashStyle='longdash'),
+                                                              list(value=48, color='#A0A0A0', width=.75, dashStyle='longdash')
                                           )))
                                   c$yAxis(title = list(text = "Percent Yield"))
                                   
@@ -41,39 +41,39 @@ shinyServer(function(input, output) {
                                  labels<-oneyearlabels
       } 
       
-                                  c <- Highcharts$new()
-                                  c$chart(type = "spline", height="450", width="800", zoomType="x")
-                                  c$title(text = paste("TSP ", input$radio, ", Past ", input$time))
-                                  c$set(dom = "chart")
-                                  
-                                  c$xAxis(title = list(text = "Month"), categories = labels)
-                                  c$yAxis(title = list(text = "Percent Yield"))
-                                  
-                                  c$exporting(enabled = T)
-                                  c$credits(text="Data by www.TSPcenter.com",href="http://www.TSPcenter.com")
-                                  
-                                  c$series(name = "G Fund", data = subdata[,1], dashStyle = "Solid", marker = list(enabled=FALSE))
-                                  c$series(name = "F Fund", data = subdata[,2], dashStyle = "Solid", marker = list(enabled=FALSE))
-                                  c$series(name = "C Fund", data = subdata[,3], dashStyle = "Solid", marker = list(enabled=FALSE))
-                                  c$series(name = "S Fund", data = subdata[,4], dashStyle = "Solid", marker = list(enabled=FALSE))
-                                  c$series(name = "I Fund", data = subdata[,5], dashStyle = "Solid", marker = list(enabled=FALSE)) 
-                                  return(c)
+      c <- Highcharts$new()
+      c$chart(type = "spline", height="500", width="900", zoomType="x")
+      c$title(text = paste("TSP ", input$radio, ", Past ", input$time))
+      c$set(dom = "chart")
+      
+      c$xAxis(title = list(text = "Month"), categories = labels)
+      c$yAxis(title = list(text = "Percent Yield"))
+      
+      c$exporting(enabled = T)
+      c$credits(text="Data by www.TSPcenter.com",href="http://www.TSPcenter.com")
+      
+      c$series(name = "G Fund", data = subdata[,1], dashStyle = "Solid", marker = list(enabled=FALSE))
+      c$series(name = "F Fund", data = subdata[,2], dashStyle = "Solid", marker = list(enabled=FALSE))
+      c$series(name = "C Fund", data = subdata[,3], dashStyle = "Solid", marker = list(enabled=FALSE))
+      c$series(name = "S Fund", data = subdata[,4], dashStyle = "Solid", marker = list(enabled=FALSE))
+      c$series(name = "I Fund", data = subdata[,5], dashStyle = "Solid", marker = list(enabled=FALSE)) 
+      return(c)
     }
     
-    else if(input$radio == "Prices"){
+    else if(input$radio == "Prices of Funds"){
       if(input$time == "5 Years"){subdata <- tspdata[,8:12][seq(1, nrow(tspdata), by=21),]; 
                                   labels<-c(fiveyearlabels,fiveyearlabels,fiveyearlabels,fiveyearlabels,fiveyearlabels,"J")
                                   
                                   c <- Highcharts$new()
-                                  c$chart(type = "spline", height="450", width="800", zoomType="x")
+                                  c$chart(type = "spline", height="500", width="900", zoomType="x")
                                   c$set(dom = "chart")    
                                   c$title(text = paste("TSP ", input$radio, ", Past ", input$time)) 
                                   
                                   c$xAxis(title = list(text = "Month"), categories = labels
-                                          , plotLines = (list(list(value=12, color='#B7CEEC', width=.75, dashStyle='longdash'),
-                                                              list(value=24, color='#B7CEEC', width=.75, dashStyle='longdash'),
-                                                              list(value=36, color='#B7CEEC', width=.75, dashStyle='longdash'),
-                                                              list(value=48, color='#B7CEEC', width=.75, dashStyle='longdash')
+                                          , plotLines = (list(list(value=12, color='#A0A0A0', width=.75, dashStyle='longdash'),
+                                                              list(value=24, color='#A0A0A0', width=.75, dashStyle='longdash'),
+                                                              list(value=36, color='#A0A0A0', width=.75, dashStyle='longdash'),
+                                                              list(value=48, color='#A0A0A0', width=.75, dashStyle='longdash')
                                           )))
                                   c$yAxis(title = list(text = "Price (dollars)"),min=0)
                                   
@@ -91,7 +91,7 @@ shinyServer(function(input, output) {
                                  labels<-oneyearlabels 
                                  
                                  c <- Highcharts$new()
-                                 c$chart(type = "spline", height="450", width="800", zoomType="x")
+                                 c$chart(type = "spline", height="500", width="900", zoomType="x")
                                  c$set(dom = "chart")    
                                  c$title(text = paste("TSP ", input$radio, ", Past ", input$time))
                                  
@@ -115,3 +115,4 @@ shinyServer(function(input, output) {
   
   
 }) #ShinyServer
+
--- ui.R
+++ ui.R
@@ -22,34 +22,34 @@ shinyUI(bootstrapPage(
     ),
     
     tags$hr(), #horizontal rule, may not render in some IE versions
-  
+    
     row(
       col( 4,tags$br()),
       col( 2, h4("Select Chart Type:", align="right")),
       
       col( 3, 
            gsub("label class=\"radio\"", "label class=\"radio inline\"", 
-                radioButtons(inputId = "radio", label="", choices = c("Yields", "Prices"))), align="left"
-        )    
-      ),
+                radioButtons(inputId = "radio", label="", choices = c("Yields of Funds", "Prices of Funds"))), align="left"
+      )    
+    ),
     
     row(
       col( 4,tags$br()),      
       col( 2, h4("Select Time Period:", align="right")),      
       col( 3, 
-                   gsub("label class=\"radio\"", "label class=\"radio inline\"", 
-                        radioButtons(inputId = "time", label="", choices = c("1 Year", "5 Years"))), align="left"
-        )
-      ),
+           gsub("label class=\"radio\"", "label class=\"radio inline\"", 
+                radioButtons(inputId = "time", label="", choices = c("1 Year", "5 Years"))), align="left"
+      )
+    ),
     
     tags$hr(),    
     
     row( 
       col( 12, align="center",
            showOutput("chart","highcharts")
-        )
-      ),
-
+      )
+    ),
+    
     tags$hr(),
     
     row(
@@ -57,12 +57,15 @@ shinyUI(bootstrapPage(
       HTML('<p style="margin:0;">This app licensed under <a href="http://www.gnu.org/licenses/gpl.html">GPL-3</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
           Hosted by <a href="aws.amazon.com"><img src="awslogo.jpg" width="7%" height="7%"></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 
            Powered by <a href="http://www.rstudio.com/shiny/"><img src="RstudioLogo.png" width="7%" height="7%"></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-             See code on <a href="https://github.com/asmith3/TSPerformance"><img src="gitlogo.png" width="5%" height="5%"></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-              v1.1
+             See code on <a href="https://github.com/asmith3"><img src="gitlogo.png" width="5%" height="5%"></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp v1.2
            ')
     )
-        
+    
   )   #tags$div 
 ) #bootstrap  
 ) #shinyUI
+
+
+
+
 
