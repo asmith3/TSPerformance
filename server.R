b4c815932394b92c9a0071c64202f0162cbf9dfe
asmith3 <baylorAsmith@gmail.com>
2014-02-11 16:21:17 +0000

    Comment changes

--- server.R
+++ server.R
@@ -1,34 +1,85 @@
-library(shiny)
-library(rCharts)
+##################################################################################################################
+#                                                                                                                #
+# This is the companion script to "ui.R", which is the other script required to run Shiny applications           #
+#                                                                                                                #
+##################################################################################################################
 
+# To learn more about the Shiny package, visit http://www.rstudio.com/shiny/
+
+# If you are new to R, check out Anthony Damico's 'two minute tutorials' at http://www.twotorials.com/
+
+# Special thanks to Ramnath Vaidyanathan (github: ramnathv) for his rCharts package
+
+#-----------------------------------------------------------------------------------------------------------------
+
+require(shiny)      # Load the shiny package
+
+require(rCharts)    # Load rCharts, the javascript wrapper for various js visualizations
+                    # more info rCharts is available at http://ramnathv.github.io/rCharts/
+
+# load the datasets comprised of yields for different thrift savings plan accounts
 load("oneyearyields.Rdb")
 load("fiveyearyields.Rdb")
 load("eightyearyields.Rdb")
 
+# although there are more thrift savings plans than listed here, this demo app only makes use of the G,F,I,C, and S funds
+
+
+# the shinyServer function in part defines what will change within your application
+
+# notice items that begin with input$ , which link to the items defined in the ui.R script
 shinyServer(function(input, output) {
   
+  
+  # the (javascript based) morris chart is the only thing that changes in the ui, so it is the only thing defined in shinyServer()
   output$chart <- renderChart({
     
+    # the base data for the graphic is assigned to an object based on which radio button is selected in the ui
     if(input$radio == "1 Year Yields") {
-      data <- oneyearyields[seq(1, nrow(oneyearyields), by=5),]    
-    }
-    if(input$radio == "5 Year Yields") {
+      data <- oneyearyields[seq(1, nrow(oneyearyields), by=5),]       # these seq() take a subset of the data in order to speed up 
+    }                                                                 # the rendering of the graphic; you can remove the subset for 
+    if(input$radio == "5 Year Yields") {                              # higher resolution
       data <- fiveyearyields[seq(1, nrow(fiveyearyields), by=21),]
     }
     if(input$radio == "8 Year Yields") {
       data <- eightyearyields[seq(1, nrow(eightyearyields), by=21),]
     }
     
+    # morris charts play nicer with your dates as character objects 
     data <- transform(data, Date = as.character(Date))
     
-    m1 <- mPlot(x = "Date", y = c("S.Fund", "C.Fund", "I.Fund", "G.Fund", "F.Fund"), type = "Line", data = data, 
-                labels = list("S Fund", "C Fund", "I Fund", "G Fund", "F Fund"))
-    m1$set(pointSize = 0, lineWidth = 1, dom = "chart", postUnits = "%", hideHover=FALSE, fillOpacity="1.0")
-    if(input$radio == "8 Year Yields") {m1$set(events=list("2008-09-01"), eventStrokeWidth = "50", eventLineColors = list("#F8F8F8"))}
-    return(m1)
+    # mPlot specifies using a morris plot in the rCharts package
+    # see the morris chart API for chart options: http://www.oesmith.co.uk/morris.js/lines.html (line and area charts)
+    #                                                  http://www.oesmith.co.uk/morris.js/bars.html (bar charts)
     
-  }) #renderchart
+    # alternatives include hPlot for highcharts, and rPlot for polycharts (via the rCharts package)
+    
+    # something didn't like spaces in list for y-values, so you can rename them directly with 'labels'
+    m1 <- mPlot(x = "Date", y = c("S.Fund", "C.Fund", "I.Fund", "G.Fund", "F.Fund"), type = "Line", data = data,
+                
+                # morris chart seems to prefer no spaces in the column names, so this line hardcodes nicer looking labels
+                labels = list("S Fund", "C Fund", "I Fund", "G Fund", "F Fund"),
+                
+                # list html color codes to replace default scheme
+                # these color choices attempt to make the chart readable to those who are color-deficient
+                lineColors = list("#006699","#FF0000","#006600","#330066","#666600")
+                )
+    
+    # use a 'set' statement to apply options found in the API. multiple set statements are allowed
+    
+    # "dom = 'chart'" seems necessary to successfully carry the chart over to the ui
+    # 'hideHover' is set to false given that it seems morris charts do not allow for static legends
+    # as seen in ui.R axis labels are hard-coded into the ui given it seems morris charts do not allow for axis labels
+      m1$set(pointSize = 0, lineWidth = 1.05, dom = "chart", postUnits = "%", hideHover=FALSE, fillOpacity="1.0")
+    
+    # this creates a recession plot band only when the user requests a time frame that includes it
+      if(input$radio == "8 Year Yields") {m1$set(events=list("2008-09-01"), eventStrokeWidth = "50", eventLineColors = list("#F8F8F8"))}
+    
+    # return the plot
+      return(m1)
+    
+  }) # end renderChart
   
   
-}) #ShinyServer
+}) #end shinyServer
 
--- ui.R
+++ ui.R
@@ -1,6 +1,23 @@
-library(shiny)
-library(rCharts)
+##################################################################################################################
+#                                                                                                                #
+#  This is the companion script to "server.R", which is the other script required to run Shiny applications      #
+#                                                                                                                #
+##################################################################################################################
 
+# To learn more about the Shiny package, visit http://www.rstudio.com/shiny/
+
+# If you are new to R, check out Anthony Damico's 'two minute tutorials' at http://www.twotorials.com/
+
+# Special thanks to Joe Cheng (github: jcheng5) for his bootstrap and css templates
+
+#-----------------------------------------------------------------------------------------------------------------
+
+library(shiny)      # Load the shiny package
+
+library(rCharts)    # Load rCharts, the javascript wrapper for various js visualizations
+                    # more info rCharts is available at http://ramnathv.github.io/rCharts/
+
+# define the functions that pass ui definitions to the bootstrapPage function
 row <- function(...) {
   tags$div(class="row", ...)
 }
@@ -9,14 +26,23 @@ col <- function(width, ...) {
   tags$div(class=paste0("span", width), ...)
 }
 
+# begin the ui definition
+
+# instead of pre-defined templates available in the shiny package (ex pageWithSidebar),
+# a bootstrap page makes use of the Twitter Bootstrap CSS framwork that allows for full control of the ui
 shinyUI(bootstrapPage(
+  
+  #"href='styles.css'" references the file in [www/] to overwrite the standard css definitions
   tags$head(tags$link(rel='stylesheet', type='text/css', href='styles.css')),
   
   tags$div(
     class = "container",
     
+    # adds a break up top for aesthetics    
     tags$p(tags$br()),
     
+    # ui definition using the bootstrapPage function uses the previously defined functions, generally nested as row(col(...),...),
+    # where row defines the next row, and column defines the next column within the row, and its span (width) as col(span,...)    
     row(
       h1('Thrift Savings Plan Account Yields',align="center")
     ),
@@ -25,25 +51,36 @@ shinyUI(bootstrapPage(
       HTML("<center><a href=http://en.wikipedia.org/wiki/Thrift_Savings_Plan>(What's a Thrift Savings Plan?)</a></center>")
     ),
     
-    tags$hr(), #horizontal rule, may not render in some IE versions
+    #horizontal rule, may not render in some IE versions
+    tags$hr(),
     
     row(
       col( 2, tags$br()),
-      col( 3, h4("Select Chart Timeline:", align="right")),
-      
+      col( 3, h4("Select Time Frame:", align="right")),      
       col( 4, 
            gsub("label class=\"radio\"", "label class=\"radio inline\"", 
                 radioButtons(inputId = "radio", label="", choices = c(paste("1 Year Yields"), paste("5 Year Yields"), paste("8 Year Yields")) )), align="left"
-      )    
+          )    
     ),
     
+    # horizontal rule
     tags$hr(),    
     
-    row( 
+    row(
+      col( 1, tags$br()),
+      
+      # hard-coded axis label, as morris chart doesn't seem to provide this
+      col( 2, h4("Percent Yield"), align = "center"),
+      
       col( 12, align="center",
+           
+           # carries morris chart over to the ui, using the 'dom=' name from the file, and the type of chart
            showOutput("chart","morris")
       )
     ),
+    
+    # hard-coded axis label, as morris chart doesn't seem to provide this
+    row(h4("Time"), align="center"),
     
     row(
       col( 8, tags$br()),
@@ -55,15 +92,15 @@ shinyUI(bootstrapPage(
     row(
       col( 3,(tags$br())),
       HTML('<p style="margin:0;">This app licensed under <a href=http://www.gnu.org/licenses/gpl.html>GPL-3</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-          Hosted by <a href=http://aws.amazon.com><img src="awslogo.jpg" width="7%" height="7%"></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 
+           Hosted by <a href=http://aws.amazon.com><img src="awslogo.jpg" width="7%" height="7%"></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 
            Powered by <a href=http://www.rstudio.com/shiny/><img src="RstudioLogo.png" width="7%" height="7%"></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-             See code on <a href=https://github.com/asmith3/TSPerformance><img src="gitlogo.png" width="5%" height="5%"></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspv1.2
+           See code on <a href=https://github.com/asmith3/TSPerformance><img src="gitlogo.png" width="5%" height="5%"></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspv1.3
            ')
-    )
+      )
     
-  )   #tags$div 
-) #bootstrap  
-) #shinyUI
+      )   #tags$div 
+    ) #bootstrap  
+    ) #shinyUI
 
 
 
